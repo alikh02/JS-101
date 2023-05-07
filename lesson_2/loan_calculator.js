0ca50a90cd394088272400c54c5e65877de556e2
Khaled Ali <rubykid21@gmail.com>
2023-05-07 10:01:50 -0400

    Fixed bug in Calculations

--- lesson_2/loan_calculator.js
+++ lesson_2/loan_calculator.js
@@ -1,14 +1,50 @@
 let rlSync = require('readline-sync');
 
+function anotherCalculation() {
+  let answer = rlSync.question("\nWould you like to make another calculation?" +
+  "\n(Y or y for Yes, otherwise any other key to quit.)  ");
+  if (answer === 'Y' ||  answer === 'y') {
+    console.log("-----------------------------------------------------\n");
+    return true;
+  }
+  return false;
+}
+
+function getMonthlyInterestRate() {
+  let interestRate = rlSync.question("Annual interest rate of the loan? (Ex. Enter 5 for 5%) ");
+  interestRate /= 100;
+
+  return interestRate / 12;
+}
+
+function getMonthlyPayment(loanAmount, loanLengthMonths, monthlyInterestRate) {
+  console.log("LOCAL VARIABLE MONTHLY INTEREST RATE ISSS: " + monthlyInterestRate);
+  let monthlyPayment = loanAmount * (monthlyInterestRate /
+  (1 - Math.pow((1 + monthlyInterestRate), (-loanLengthMonths))));
+
+  return monthlyPayment;
+}
+
 console.log("WELCOME TO LOAN CALCULATOR!");
-let loanAmount = rlSync.question("Enter loan amount: \n");
-console.log(`Nice! okay so your loan amount is ${loanAmount}`);
-let loanLengthYears = rlSync.question("Enter the loan duration (in years): \n");
+console.log("--------------------------");
+let isFinished = false;
 
-let monthlyInterestRate = 0.06;
+while (!isFinished) {
+  let loanAmount = rlSync.question("Enter loan amount: \n");
+  let loanLengthMonths = rlSync.question("Enter the loan duration (in months): \n");
 
-let monthlyPayment = loanAmount * (monthlyInterestRate /
- (1 - Math.pow((1 + monthlyInterestRate), (-loanLengthYears))) / 12);
+  // Returns the monthly payment
+  // eslint-disable-next-line max-len
+  let monthlyPayment = getMonthlyPayment(loanAmount, loanLengthMonths, getMonthlyInterestRate());
 
-console.log("Thank you.. Your monthly payment is: " + monthlyPayment);
-console.log("Total of " + (loanLengthYears * 12) + " payments is: " + (monthlyPayment * loanLengthYears * 12));
+
+  let totalPayment = (monthlyPayment * loanLengthMonths).toFixed(2);
+
+  console.log("Thank you.. Your monthly payment is: $" + monthlyPayment.toFixed(2));
+  console.log("Total of " + (loanLengthMonths) + " payments is: $" + totalPayment);
+
+  if (!anotherCalculation()) {
+    isFinished = true;
+  }
+}
+console.log("Thanks for using Loan Calculator! ");
